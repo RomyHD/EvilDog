ff3c73ad8fb0f10eab4ee29d01af3e52acdb9ba4
Vardey <vardey2009@gmail.com>
2024-11-20 11:44:33 +0700

    12th commit

--- index2.js
+++ index2.js
@@ -25,3 +25,21 @@ function getDOB() {
     // Display the result
     document.getElementById('currentAge').textContent = `Umur anda adalah ${age} tahun.`;
 }
+const menu = document.getElementById("menu");
+const closeButton = document.getElementById("close-mobile");
+const nav = document.getElementById("nav-mobile");
+const navLink = document.querySelectorAll(".nav-link");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("show");
+});
+
+closeButton.addEventListener("click", () => {
+  nav.classList.remove("show");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+});
